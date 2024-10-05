c69776fd282a7165b96b64a4d657d6ee074974fb
Min Kim <minkimcello@gmail.com>
2024-10-05 19:21:46 -0400

    Organize and rename variables

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { graphql } from "@octokit/graphql";
-import { Comment, Discussion, DiscussionComment } from './__generated__/types'; // 🚨 hmmm
+import { DiscussionComment } from './__generated__/types';
 import lodash from "lodash";
 import {
   RateLimit,
@@ -9,7 +9,7 @@ import {
 import {
   DiscussionsQuery,
   CommentsQuery,
-  RepliesQuery,
+  // RepliesQuery, // 🚨 see comments below
 } from './queries.__generated__';
 import {
   DISCUSSIONS_QUERY,
@@ -30,163 +30,168 @@ async function execute() {
   const repo = "next.js";
   const org = "vercel";
 
-  let bookkeeping = [] as RateLimit[];
-  let totalbook = [] as RateLimit[];
-  const ah = [] as any[];
+  let discussionBatchCost = [] as RateLimit[];
+  let totalCost = [] as RateLimit[];
+  const results = [] as any[];
 
-  // 🟢 loop discussion batch
-  let hasNext = true;
-  let afterz = "";
+  let moreDiscussions = true;
+  let lastDiscussionCursor = "";
   try {
-    while(hasNext) {
+    while(moreDiscussions) {
       const {
         repository,
         rateLimit,
       } = await client<DiscussionsQuery>(DISCUSSIONS_QUERY, {
         name: repo,
         owner: org,
-        after: afterz,
+        after: lastDiscussionCursor,
       });
       if (rateLimit) {
-        bookkeeping.push(rateLimit);
+        // cost for querying 100 discussions and first 100
+        // comments (id, body, author) of each discussion - without replies
+        discussionBatchCost.push(rateLimit);
       }
-      const nodes = repository?.discussions?.nodes;
-      if (nodes && nodes.length) {
-        let discussionNumbers = nodes.reduce((acc, node) => {
-          if (node) {
-            return [...acc, node.number];
+      const discussions = repository?.discussions?.nodes;
+
+      if (discussions && discussions.length) {
+        console.log("Querying:", discussions.reduce((acc, discussion) => {
+          if (discussion) {
+            return [...acc, discussion.number];
           }
           return acc;
-        }, [] as number[])
-        console.log("Querying", discussionNumbers);
-        for(const node of nodes) {
-          const {
-            title,
-            url,
-            bodyText,
-            number,
-            author,
-            category,
-            comments,
-          } = node as Discussion;
-  
-          let discussion = {
-            title,
-            url,
-            bodyText,
-            number,
-            author: author?.login,
-            category: category?.name,
-            comments: comments.nodes ? [...comments.nodes] : [],
-          };
-  
-          let nextPage = comments.pageInfo.hasNextPage;
-          let after = comments.pageInfo.endCursor;
-  
-          while(nextPage) {
-            const {
-              repository,
-              rateLimit,
-            } = await client<CommentsQuery>(COMMENTS_QUERY, {
-              name: repo,
-              owner: org,
-              number,
-              after,
-            });
-            if (rateLimit) {
-              bookkeeping.push(rateLimit)
-            }
-            const {
-              comments,
-            } = repository?.discussion as Discussion;
-            if (comments.nodes) {
-              comments.nodes.forEach(comment => {
-                discussion.comments.push(comment);
+        }, [] as number[]));
+
+        for(const _discussion of discussions) {
+          if (_discussion) {
+            const discussion = {
+              title: _discussion.title,
+              url: _discussion.url,
+              bodyText: _discussion.bodyText,
+              number: _discussion.number,
+              author: _discussion.author?.login,
+              category: _discussion.category?.name,
+              comments: _discussion.comments.nodes ? [..._discussion.comments.nodes] : [],
+            };
+   
+            let moreComments = _discussion.comments.pageInfo.hasNextPage;
+            let lastCommentsCursor = _discussion.comments.pageInfo.endCursor;
+    
+            // only loops if there are more than 100 comments in the discussion
+            // shouldn't need to queue this up into batches as there probably
+            // won't be that many discussions with more than 100 comments
+            while(moreComments) {
+              const {
+                repository,
+                rateLimit,
+              } = await client<CommentsQuery>(COMMENTS_QUERY, {
+                name: repo,
+                owner: org,
+                number: discussion.number,
+                after: lastCommentsCursor,
               });
-            }
-            after = comments.pageInfo.endCursor;
-            nextPage = false; // comments.pageInfo.hasNextPage
-          }
-  
-          let commentsBatches = lodash.chunk(discussion.comments, 50);
-          if (commentsBatches.length > 1) {
-            let lastBatch = commentsBatches.pop();
-            if ((lastBatch.length / commentsBatches.length) < 5) {
-              lastBatch.forEach((comment, index) => {
-                commentsBatches[index % commentsBatches.length].push(comment);
-              });
-            } else {
-              commentsBatches.push(lastBatch);
-            }  
-          }
-          for (const commentBatch of commentsBatches) {
-            const BATCH_QUERY = generateBatchQuery(commentBatch);
-            const batch: any = await client(BATCH_QUERY); // 🚨 type
-            
-            const rateLimit = batch.rateLimit;
-            delete batch.rateLimit;
-            
-            bookkeeping.push(rateLimit);
-  
-            const batchSplit: [ // 🚨 type
-              string,
-              {
-                id,
-                replies: {
-                  pageInfo: {
-                    hasNextPage: boolean,
-                    endCursor: string,
-                  },
-                  nodes: DiscussionComment[],
-                }
+              if (rateLimit) {
+                discussionBatchCost.push(rateLimit)
               }
-            ][] = Object.entries(batch);
-  
-            for (const [_, result] of batchSplit) {
-              let hasNextPageOfReplies = result.replies.pageInfo.hasNextPage;
-              let cursor = result.replies.pageInfo.endCursor;
-              while (hasNextPageOfReplies) {
-                if (result.replies.pageInfo.hasNextPage) {
-                  const { node: { replies }, rateLimit }: Replies = await client(REPLIES_QUERY, {
-                    id: result.id,
-                    after: cursor,
-                  });
-                  bookkeeping.push(rateLimit);
-                  if (replies.nodes && replies.nodes.length) {
-                    result.replies.nodes.push(...replies.nodes);
+              if (repository?.discussion?.comments.nodes) {
+                repository?.discussion?.comments.nodes.forEach(comment => {
+                  discussion.comments.push(comment);
+                });
+              }
+              lastCommentsCursor = repository?.discussion?.comments.pageInfo.endCursor;
+              moreComments = false; // 🚨 repository?.discussion?.comments.pageInfo.hasNextPage
+            }
+
+            // 🚨 TODO: aggregate comments so that each bulk request fills up to the max size
+            let commentsBatches = lodash.chunk(discussion.comments, 50);
+
+            // 🔴 this will become redundant if we fill up to the max size
+            if (commentsBatches.length > 1) {
+              let lastBatch = commentsBatches.pop();
+              if ((lastBatch.length / commentsBatches.length) < 5) {
+                lastBatch.forEach((comment, index) => {
+                  commentsBatches[index % commentsBatches.length].push(comment);
+                });
+              } else {
+                commentsBatches.push(lastBatch);
+              }
+            }
+            // 🔴
+            
+            for (const commentBatch of commentsBatches) {
+              const batchResults: any = await client(generateBatchQuery(commentBatch)); // 🚨 type
+              
+              discussionBatchCost.push(batchResults.rateLimit);
+              delete batchResults.rateLimit;
+    
+              const batchSplit: [ // 🚨 type
+                string,
+                {
+                  id,
+                  replies: {
+                    pageInfo: {
+                      hasNextPage: boolean,
+                      endCursor: string,
+                    },
+                    nodes: DiscussionComment[],
                   }
-                  hasNextPageOfReplies = replies.pageInfo.hasNextPage;
-                  cursor = replies.pageInfo.endCursor;
-                } else {
-                  hasNextPageOfReplies = false;
+                }
+              ][] = Object.entries(batchResults);
+              
+              for (const [_, comment] of batchSplit) {
+                let moreReplies = comment.replies.pageInfo.hasNextPage;
+                let lastRepliesCursor = comment.replies.pageInfo.endCursor;
+
+                // only loops if there are more than 100 replies in a comment
+                while (moreReplies) {
+                  // 🚨 can't use generated RepliesQuery from __generated because it doesn't recognize `... on DiscussionComment`: says `replies` does not exist
+                  const { node: { replies }, rateLimit }: Replies = await client(REPLIES_QUERY, {
+                    id: comment.id,
+                    after: lastRepliesCursor,
+                  });
+                  discussionBatchCost.push(rateLimit);
+
+                  if (replies.nodes && replies.nodes.length) {
+                    comment.replies.nodes.push(...replies.nodes);
+                  }
+                  moreReplies = replies.pageInfo.hasNextPage;
+                  lastRepliesCursor = replies.pageInfo.endCursor;
+                }
+                const discussionComment = discussion.comments.find(_comment => _comment?.id === comment.id) as DiscussionComment;
+                if (discussionComment) {
+                  discussionComment.replies = {
+                    nodes: comment.replies.nodes,
+                    totalCount: 0,
+                    pageInfo: {
+                      hasPreviousPage: false,
+                      hasNextPage: false,
+                      endCursor: ""
+                    }
+                  };
                 }
               }
-              const discussionComment = discussion.comments.find(comment => comment?.id === result.id);
-              if (discussionComment) {
-                discussionComment.replies = { nodes: result.replies.nodes, totalCount: 0, pageInfo: { hasPreviousPage: false, hasNextPage: false, endCursor: "" } };
-              }
             }
+            results.push(discussion);
           }
-          ah.push(discussion);
         }
       }
-      afterz = repository?.discussions.pageInfo.endCursor || "";
+      lastDiscussionCursor = `${repository?.discussions.pageInfo.endCursor}`;
       if (!repository?.discussions.pageInfo.hasNextPage) {
-        hasNext = false
+        moreDiscussions = false
       }
-      console.log(tally(bookkeeping));
-      totalbook = [...bookkeeping];
-      bookkeeping = [];
+      console.log(tally(discussionBatchCost));
+      totalCost = [...discussionBatchCost];
+      discussionBatchCost = [];
     }
-    fs.writeFileSync('test.json', JSON.stringify({ data: ah }, null, 2))
-  } catch(e) {
-    console.log("something went wrong", e);
+    fs.writeFileSync('test.json', JSON.stringify({ data: results }, null, 2));
+  } catch(err) {
+    console.error(err);
   } finally {
-    console.log(tally(totalbook));
+    console.log(tally(totalCost));
   }
 }
 
-interface Replies {
+// 🚨 can't use generated RepliesQuery from __generated because it doesn't recognize `... on DiscussionComment`: says `replies` does not exist
+interface Replies { 
   node: {
     replies: {
       nodes:  DiscussionComment[];
